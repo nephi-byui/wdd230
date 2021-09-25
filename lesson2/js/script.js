39618c3f9c72f1f11988ec5906c4ce81e43888c9
nephi-byui <byui@nephi.malit.me>
2021-09-25 12:16:43 +0800

    renamed

--- lesson2/js/script.js
+++
@@ -1,9 +0,0 @@
-const yearElement = document.querySelector('#footer-year');
-
-function setCopyrightDate() {
-    date = new Date();
-    yearElement.innerHTML = date.getFullYear();
-};
-
-window.onload = setCopyrightDate() 
-;
