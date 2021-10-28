bb196983f2cd2e4a2d5d734351469737e5946743
nephi-byui <byui@nephi.malit.me>
2021-10-28 23:18:34 +0800

    added conditions to visit counter

--- lesson7/js/days_ago.js
+++ lesson7/js/days_ago.js
@@ -1,4 +1,7 @@
 const firstVisitRemove = document.querySelector('.clear-first-time')
+const hitCounterSentence = document.querySelector('.hitcounter-sentence')
+const visitCount = document.querySelector('.visit-count')
+
 const daysAgo = document.querySelector('.days-ago');
 const thisVisit = document.querySelector('.this-visit');
 const lastVisit = document.querySelector('.last-visit');
@@ -9,30 +12,35 @@ function days_ago() {
 
     // check today's date
     this_visit = new Date()
+    this_visit_string = this_visit.toLocaleDateString(undefined, display_options);
 
     // get last visit date
     stored_value = localStorage.getItem("last-visit");
-    //int = parseInt(stored_value)
-    //last_visit = new Date(parseInt(localStorage.getItem("last-visit"))); 
-    last_visit = new Date(stored_value)
 
-    // if this is the first time to visit, display a message.
-    if(last_visit == null) {
+    if (stored_value != null) {
+        last_visit = new Date(stored_value);
+        last_visit_string = last_visit.toLocaleDateString(undefined, display_options);
+    };
+
+    // if this is the first time to visit
+    if (stored_value == null) {
+
+        // display current date
+        thisVisit.innerHTML = this_visit_string
+
+        // hide irrelevant data
+        firstVisitRemove.innerHTML = null
+
+        // add to hitcounter
+        localStorage["visit-counter"] = 1
 
         // display a message
-        firstVisitRemove.innerHTML = ""
+        hitCounterSentence.innerHTML = "According to your browser, this is your first visit. Welcome! ";
         
         // store time of visit
         localStorage.setItem("last-visit", this_visit)
 
     } else {
-
-
-        //last_visit_string = last_visit.toLocaleDateString(undefined, display_options);
-
-        // generate strings
-        this_visit_string = this_visit.toLocaleDateString(undefined, display_options);
-        last_visit_string = last_visit.toLocaleDateString(undefined, display_options);
 
         // display today's date.
         thisVisit.innerHTML = this_visit_string
@@ -41,17 +49,30 @@ function days_ago() {
         lastVisit.innerHTML = last_visit_string;
 
         // compute the days passed
-        milliseconds_passed = this_visit - last_visit
-        days_passed_float = milliseconds_passed / 86400000
-        days_passed = days_passed_float.toFixed(0);
+        milliseconds_passed = this_visit - last_visit;
+        days_passed_float = milliseconds_passed / 86400000;
 
-        // retrieve the last visited date and display it:
+        // convert it into a string to be displayed and display it
+        days_passed = days_passed_float.toFixed(0);
         daysAgo.innerHTML = days_passed;
         
         //set the new last visited date
         localStorage["last-visit"] = this_visit
-    }
 
+        // reset null or NaN visit counter
+        if (localStorage["visit-counter"] == null) {
+            localStorage["visit-counter"] = 1
+        } else if (localStorage["visit-counter"] == "NaN") {
+            localStorage["visit-counter"] = 1
+        };
+    
+        // update visit counter +1
+        localStorage.setItem("visit-counter",
+                            (parseInt(localStorage["visit-counter"]) + 1));
+
+        // display visit counter new value
+        visitCount.innerHTML = localStorage["visit-counter"]
+    }
 };
 
 days_ago();
