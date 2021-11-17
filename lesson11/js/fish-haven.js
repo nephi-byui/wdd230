e3ae04ad03722025cc5496c3a19beeed71820f10
nephi-byui <byui@nephi.malit.me>
2021-11-17 23:38:08 +0800

    all pages complete

---
+++ lesson11/js/fish-haven.js
@@ -0,0 +1,7 @@
+const PRESTON = 5604473
+const SODA_SPRINGS = 5607916
+const FISH_HAVEN = 5585010
+
+get_weather_summary_for(FISH_HAVEN)
+get_forecast_for(FISH_HAVEN)
+get_events_for('Fish Haven')
---
+++ lesson11/js/soda-springs.js
@@ -0,0 +1,7 @@
+const PRESTON = 5604473
+const SODA_SPRINGS = 5607916
+const FISH_HAVEN = 5585010
+
+get_weather_summary_for(SODA_SPRINGS)
+get_forecast_for(SODA_SPRINGS)
+get_events_for('Soda Springs')
